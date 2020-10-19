7bf71a1718641957767b631fe4ec7247578224c9
xiami <xiami@amazon.com>
2020-10-19 14:38:28 -0700

    AWS Otel collector StastD branch

--- pkg/defaultcomponents/defaults.go
+++ pkg/defaultcomponents/defaults.go
@@ -18,6 +18,7 @@ package defaultcomponents
 import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awsemfexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awsxrayexporter"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/statsdreceiver"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenterror"
 	"go.opentelemetry.io/collector/exporter/fileexporter"
@@ -41,6 +42,7 @@ func Components() (component.Factories, error) {
 	factories.Receivers, err = component.MakeReceiverFactoryMap(
 	    prometheusreceiver.NewFactory(),
 	    otlpreceiver.NewFactory(),
+		statsdreceiver.NewFactory(),
 	)
 	if err != nil {
 		errs = append(errs, err)
