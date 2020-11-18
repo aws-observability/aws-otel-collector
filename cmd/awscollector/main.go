2c88e41a1b69ab9ac65759bd002b917f034a4413
Min Xia <xiami@amazon.com>
2020-11-18 11:16:04 -0800

    fix the new breaking zap logger hook (#135)

--- cmd/awscollector/main.go
+++ cmd/awscollector/main.go
@@ -25,7 +25,7 @@ import (
 	"aws-observability.io/collector/tools/version"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/service"
-	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap"
 )
 
 // aws-otel-collector is built upon opentelemetry-collector.
@@ -57,7 +57,7 @@ func main() {
 		Factories:            factories,
 		ApplicationStartInfo: info,
 		ConfigFactory:        cfgFactory,
-		LoggingHooks:         []func(entry zapcore.Entry) error{lumberHook}}); err != nil {
+		LoggingOptions:       []zap.Option{zap.Hooks(lumberHook)}}); err != nil {
 		log.Fatal(err)
 	}
 
