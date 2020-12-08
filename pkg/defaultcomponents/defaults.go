148e432d291c767f342a80eb1066fee8b9f04f1e
xiami <xiami@amazon.com>
2020-12-07 21:01:29 -0800

    trim the unexpected components for the collector release

--- pkg/defaultcomponents/defaults.go
+++ pkg/defaultcomponents/defaults.go
@@ -22,10 +22,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/datadogexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/dynatraceexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/newrelicexporter"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/logzioexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/sapmexporter"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/signalfxexporter"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/splunkhecexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricstransformprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awsecscontainermetricsreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awsxrayreceiver"
@@ -83,11 +80,8 @@ func Components() (component.Factories, error) {
 		otlphttpexporter.NewFactory(),
 		dynatraceexporter.NewFactory(),
 		sapmexporter.NewFactory(),
-		signalfxexporter.NewFactory(),
-		splunkhecexporter.NewFactory(),
 		datadogexporter.NewFactory(),
 		newrelicexporter.NewFactory(),
-		logzioexporter.NewFactory(),
 		awsprometheusremotewriteexporter.NewFactory(),
 	)
 	if err != nil {
