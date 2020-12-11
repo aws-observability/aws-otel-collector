786233ed60479a4141185c1f49cac9919a51f0a9
Ying <wangmyin@amazon.com>
2020-12-11 14:32:30 -0800

    Update create_rpm.sh

--- tools/packaging/linux/create_rpm.sh
+++ tools/packaging/linux/create_rpm.sh
@@ -21,7 +21,8 @@ echo "**********************************************************"
 SPEC_FILE="tools/packaging/linux/build.spec"
 BUILD_ROOT="`pwd`/build/rpmbuild"
 WORK_DIR="`pwd`/build/rpmtar"
-VERSION=`cat VERSION`
+# change replace - with ~ since rpm doesn't support ~.
+VERSION=`cat VERSION | sed 's/\-/~/g'`
 RPM_NAME=aws-otel-collector
 AOC_ROOT=${WORK_DIR}/${RPM_NAME}-${VERSION}
 
@@ -77,8 +78,7 @@ mv ${RPM_NAME}-${VERSION}.tar.gz ${BUILD_ROOT}/SOURCES/
 rm -rf ${WORK_DIR}
 
 echo "Creating the rpm package"
-# change replace - with ~ since rpm doesn't support ~.
-VERSION=`echo '$VERSION' | sed 's/\-/~/g'`
+
 rpmbuild --define "VERSION $VERSION" --define "RPM_NAME $RPM_NAME" --define "_topdir ${BUILD_ROOT}" --define "_source_filedigest_algorithm 8" --define "_binary_filedigest_algorithm 8" -bb -v --clean ${SPEC_FILE} --target ${ARCH}-linux
 
 echo "Copy rpm file to ${DEST}"
