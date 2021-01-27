1d15cc165f781563f6b0509a8d84f5f40e5d2dc9
Vastin <heayh@amazon.com>
2021-01-27 12:04:58 -0600

    - Bump version of OpenTelemetry Collector to v0.19.0 (#320)
    - Remove queued_retry processor from unit test

--- pkg/config/config_factory_test.go
+++ pkg/config/config_factory_test.go
@@ -45,7 +45,7 @@ func TestGetCfgFactoryWithoutConfig(t *testing.T) {
 }
 
 func TestGetCfgFactoryContainer(t *testing.T) {
-	os.Setenv("AOT_CONFIG_CONTENT", "extensions:\n  health_check:\n  pprof:\n    endpoint: 0.0.0.0:1777\nreceivers:\n  otlp:\n    protocols:\n      grpc:\n        endpoint: 0.0.0.0:55680\nprocessors:\n  batch:\n  queued_retry:\nexporters:\n  logging:\n    loglevel: debug\n  awsxray:\n    local_mode: true\n    region: 'us-west-2'\n  awsemf:\n    region: 'us-west-2'\nservice:\n  pipelines:\n    traces:\n      receivers: [prometheusreceiver]\n      exporters: [logging,awsxray]\n    metrics:\n      receivers: [prometheusreceiver]\n      exporters: [awsemf]\n  extensions: [pprof]")
+	os.Setenv("AOT_CONFIG_CONTENT", "extensions:\n  health_check:\n  pprof:\n    endpoint: 0.0.0.0:1777\nreceivers:\n  otlp:\n    protocols:\n      grpc:\n        endpoint: 0.0.0.0:55680\nprocessors:\n  batch:\nexporters:\n  logging:\n    loglevel: debug\n  awsxray:\n    local_mode: true\n    region: 'us-west-2'\n  awsemf:\n    region: 'us-west-2'\nservice:\n  pipelines:\n    traces:\n      receivers: [prometheusreceiver]\n      exporters: [logging,awsxray]\n    metrics:\n      receivers: [prometheusreceiver]\n      exporters: [awsemf]\n  extensions: [pprof]")
 	defer os.Unsetenv("AOT_CONFIG_CONTENT")
 	v := config.NewViper()
 	cmd := &cobra.Command{}
@@ -63,6 +63,5 @@ func TestGetCfgFactoryContainer(t *testing.T) {
 	assert.True(t, cfgModel.Receivers != nil && cfgModel.Receivers["otlp"] != nil)
 	assert.True(t, cfgModel.Receivers != nil && cfgModel.Receivers["prometheus"] == nil)
 	assert.True(t, cfgModel.Exporters != nil && cfgModel.Exporters["awsemf"] != nil)
-	assert.True(t, cfgModel.Processors != nil && cfgModel.Processors["queued_retry"] != nil)
 	assert.True(t, cfgModel.Processors != nil && cfgModel.Extensions["pprof"] != nil)
 }
