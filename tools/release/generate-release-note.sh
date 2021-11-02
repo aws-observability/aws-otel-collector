07a8b2376a7946d400c15e048ea41a229278bb20
Jeffrey Chien <chienjef@amazon.com>
2021-11-02 14:27:20 -0400

    Added comments describing formatting choices.

--- tools/release/generate-release-note.sh
+++ tools/release/generate-release-note.sh
@@ -17,6 +17,8 @@ VERSION=$1
 
 sed "s/__VERSION__/$VERSION/g" tools/release/header.md.template > header
 sed "s/__VERSION__/$VERSION/g" tools/release/downloading-links.md.template > downloading-links
+# Formats changelog sections: **<label>:** becomes ### <label>
+# Skips the first 4 lines (title and version number)
 sed -e 1,4d -e "s/^\*\*\(.*\)\:\*\*/### \1/g" docs/releases/${VERSION}.md > changelog
 
 cat header changelog downloading-links > release-note
