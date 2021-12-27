0e168561ad49ab777c833aaab9e22345f1fafe90
Khanh Nguyen <91758108+khanhntd@users.noreply.github.com>
2021-12-27 17:21:28 -0500

    Support roll back for CD when validation test are failing (#823)
    * Add delete image tag for ecr and also image tag for docker and only …
    a268451
    
    …tag lastest when success
    
    * move compare version to release-latest-image
    
    * fix some typo from cd
    
    * fix some typo from cd
    
    * fix comment when action checkout
    
    * dynamic error for delete image and also log in dockerhub
    
    * add successful respond when deleting
    
    * add check dependencies and function for deleting s3 release
    
    * add checking variable input for script
    
    * add coment for cd

---
+++ tools/release/image-binary-release/delete-docker-image-tag.sh
@@ -0,0 +1,101 @@
+#! /bin/bash
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+set -e
+
+##########################################
+# This script is used in CD workflow
+# to delete image tags from docker hub registry
+# when the validation test in CD are failing
+##########################################
+
+#Define variables to use in the environment
+#version=`cat ${local_packages_home}/VERSION`
+dockerhub_url="https://hub.docker.com"
+
+function error_exit() {
+  echo "$1" 1>&2
+  exit 1
+}
+
+function check_deps() {
+  test -f $(which jq) || error_exit "jq command not detected in path, please install it"
+  test -f $(which curl) || error_exit "curl command not detected in path, please install it"
+}
+
+function parse_environment_input(){
+  if [[ -z "${dockerhub_username}" ]]; then
+    error_exit "Missing input for flag dockerhub_username";
+  fi
+
+  if [[ -z "${dockerhub_password}" ]]; then
+    error_exit "Missing input for flag dockerhub_password";
+  fi
+
+  if [[ -z "${image_namespace}" ]]; then
+    error_exit "Missing input for flag image_namespace";
+  fi
+
+  if [[ -z "${image_repo}" ]]; then
+    error_exit "Missing input for image_repo version";
+  fi
+
+  if [[ -z "${version}" ]]; then
+    error_exit "Missing input for flag version";
+  fi
+}
+
+function log_in_dockerhub(){
+  #Get access token from docker hub to authorize when deleting
+  dockerhub_log_in_respond=$(curl -s -X "POST" \
+    --retry 2 --retry-delay 5 --retry-max-time 30 --connect-timeout 5 --max-time 10 \
+    -H "Content-Type: application/json" \
+    -d "{\"username\": \"${dockerhub_username}\", \"password\": \"${dockerhub_password}\"}" \
+    "${dockerhub_url}/v2/users/login/")
+  dockerhub_log_in_error=$(jq -r '.detail' <<< ${dockerhub_log_in_respond})
+
+  #Checking if there is any error when delete (faster failing)
+  if [[ "${dockerhub_log_in_error}" != 'null'  ]]; then
+    error_exit "Cannot log in because of error: ${dockerhub_log_in_error}"
+  fi
+
+  dockerhub_token=$(jq -r '.token' <<< ${dockerhub_log_in_respond})
+
+}
+
+function delete_image_from_dockerhub(){
+  #Delete the image from docker hub's registry
+  dockerhub_delete_respond=$(curl -L -X "DELETE" \
+    --retry 2 --retry-delay 5 --retry-max-time 30 --connect-timeout 5 --max-time 10 \
+    -H "Accept: application/json" \
+    -H "Authorization: JWT $dockerhub_token" \
+    "${dockerhub_url}/v2/repositories/${image_namespace}/${image_repo}/tags/${version}/" | jq -r '.detail')
+
+  if [[ "${dockerhub_delete_respond}" != ''  ]]; then
+    error_exit "Cannot delete because of error: ${dockerhub_delete_respond}"
+    exit 1
+  fi
+
+  echo "Delete successfully the image ${image_namespace}/${image_repo}:${version}"
+}
+
+check_deps
+parse_environment_input
+log_in_dockerhub
+delete_image_from_dockerhub
+
+
+
+
---
+++ tools/release/image-binary-release/delete-s3-release.sh
@@ -0,0 +1,87 @@
+#! /bin/bash
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+set -e
+
+##########################################
+# This script is used in CD workflow
+# to delete binaries which were uploaded to S3
+# when the validation test in CD are failing
+##########################################
+
+#Define variables to use in the environment
+s3_bucket_name="aws-otel-ss"
+package_name="aws-otel-collector"
+
+function error_exit() {
+  echo "$1" 1>&2
+  exit 1
+}
+
+function check_deps() {
+  test -f $(which aws) || error_exit "aws command not detected in path, please install it"
+}
+
+function parse_environment_input(){
+  if [[ -z "${version}" ]]; then
+    error_exit "Missing input for flag version";
+  fi
+}
+
+function delete_s3_objects_from_s3_bucket(){
+  # Get the path key for each binary and delete based on these keys
+  declare -a s3_path=(
+  	"amazon_linux/amd64/latest/${package_name}.rpm"
+  	"redhat/amd64/latest/${package_name}.rpm"
+  	"centos/amd64/latest/${package_name}.rpm"
+  	"suse/amd64/latest/${package_name}.rpm"
+  	"ubuntu/amd64/latest/${package_name}.deb"
+  	"debian/amd64/latest/${package_name}.deb"
+  	"windows/amd64/latest/${package_name}.msi"
+  	"amazon_linux/arm64/latest/${package_name}.rpm"
+  	"redhat/arm64/latest/${package_name}.rpm"
+  	"centos/arm64/latest/${package_name}.rpm"
+  	"suse/arm64/latest/${package_name}.rpm"
+  	"ubuntu/arm64/latest/${package_name}.deb"
+  	"debian/arm64/latest/${package_name}.deb"
+  )
+
+
+  for i in "${s3_path[@]}"
+  do
+  	s3_latest_key=`echo "${i}"`
+  	s3_version_key=`echo ${s3_latest_key} | sed s/latest/${version}/g`
+  	s3_version_url="s3://${s3_bucket_name}/${s3_version_key}"
+
+  	echo "Check if package is there: ${s3_version_url}"
+    aws s3api head-object --bucket "${s3_bucket_name}" --key "${s3_version_key}" > /dev/null || not_exist=true
+
+    if [ ${not_exist} ]; then
+    	error_exit "Package ${s3_version_url} is not there to delete"
+    else
+      echo "Begin to delete ${s3_version_url}"
+      aws s3 rm "${s3_version_url}"
+    fi
+
+  done
+
+  echo "Finish deleting script"
+}
+
+
+check_deps
+parse_environment_input
+delete_s3_objects_from_s3_bucket
+
