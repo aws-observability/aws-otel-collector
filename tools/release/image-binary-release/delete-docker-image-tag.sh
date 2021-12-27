e503d800254f7a25c02bd46174d4c8c01f1ffdeb
Khanh Nguyen <khanhntd@amazon.com>
2021-12-27 14:49:18 -0600

    dynamic error for delete image and also log in dockerhub

--- tools/release/image-binary-release/delete-docker-image-tag.sh
+++ tools/release/image-binary-release/delete-docker-image-tag.sh
@@ -25,20 +25,52 @@ set -e
 #version=`cat ${local_packages_home}/VERSION`
 dockerhub_url="https://hub.docker.com"
 
-#Get access token from docker hub to authorize when deleting
-dockerhub_token=$(curl -s -X "POST" \
-  -H "Content-Type: application/json" \
-  -d "{\"username\": \"${dockerhub_username}\", \"password\": \"${dockerhub_password}\"}" \
-  "${dockerhub_url}/v2/users/login/" | jq -r .token)
-
-#Checking if the input username or input password is invalid (faster failing)
-if [ ${dockerhub_token} == "null" ];  then
-  echo "Username or password is not valid."
+function error_exit() {
+  echo "$1" 1>&2
   exit 1
-fi
+}
 
-#Delete the image from docker hub's registry
-curl -i -L -X "DELETE" \
-  -H "Accept: application/json" \
-  -H "Authorization: JWT $dockerhub_token" \
-  "${dockerhub_url}/v2/repositories/${image_namespace}/${image_repo}/tags/${version}/"
+function check_deps() {
+  test -f $(which jq) || error_exit "jq command not detected in path, please install it"
+  test -f $(which curl) || error_exit "curl command not detected in path, please install it"
+}
+
+function log_in_dockerhub(){
+  #Get access token from docker hub to authorize when deleting
+  dockerhub_log_in_respond=$(curl -s -X "POST" \
+    --retry 2 --retry-delay 5 --retry-max-time 30 --connect-timeout 5 --max-time 10 \
+    -H "Content-Type: application/json" \
+    -d "{\"username\": \"${dockerhub_username}\", \"password\": \"${dockerhub_password}\"}" \
+    "${dockerhub_url}/v2/users/login/")
+  dockerhub_log_in_error=$(jq -r '.detail' <<< ${dockerhub_log_in_respond})
+
+  #Checking if there is any error when delete (faster failing)
+  if [[ "${dockerhub_log_in_error}" != 'null'  ]]; then
+    error_exit "Cannot log in because of error: ${dockerhub_log_in_error}"
+  fi
+
+  export dockerhub_token=$(jq -r '.token' <<< ${dockerhub_log_in_respond})
+
+}
+
+function delete_image_from_dockerhub(){
+  #Delete the image from docker hub's registry
+  dockerhub_delete_respond=$(curl -L -X "DELETE" \
+    --retry 2 --retry-delay 5 --retry-max-time 30 --connect-timeout 5 --max-time 10 \
+    -H "Accept: application/json" \
+    -H "Authorization: JWT $dockerhub_token" \
+    "${dockerhub_url}/v2/repositories/${image_namespace}/${image_repo}/tags/${version}/" | jq -r '.detail')
+
+  if [[ "${dockerhub_delete_respond}" != ''  ]]; then
+    error_exit "Cannot delete because of error: ${dockerhub_delete_respond}"
+    exit 1
+  fi
+}
+
+check_deps
+log_in_dockerhub
+delete_image_from_dockerhub
+
+
+
+
