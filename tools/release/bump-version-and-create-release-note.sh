1807ea3b4243b58d6a494904735d9ae3012702ac
Jeffrey Chien <chienjef@amazon.com>
2022-01-14 19:26:53 -0500

    Fix performance model version.
    Clean up release-notes scripts.

--- tools/release/bump-version-and-create-release-note.sh
+++ tools/release/bump-version-and-create-release-note.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
@@ -11,30 +13,28 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-#! /bin/bash
-
 ## this is the script to bump the version and create the release note
 ## please run this script whenever you want to bump the version instead of directly modifying the VERSION file
 ## below is an example to run this script: 
 ## RELEASE_VERSION=v0.1.8 GITHUB_TOKEN=e75***********fa3d0d ./tools/release/bump-version-and-create-release-note.sh
 
 # get the current version
-VERSION=`cat VERSION`
+VERSION=$(cat VERSION)
 OUTPUT="docs/releases/${RELEASE_VERSION}.md"
 
 # generate release note
-docker run --rm -it -v "`pwd`":/usr/local/src/your-app ferrarimarco/github-changelog-generator \
+docker run --rm -it -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator \
   --user aws-observability \
 	--project aws-otel-collector \
-	-t ${GITHUB_TOKEN} \
-	--since-tag ${VERSION} \
-	--future-release ${RELEASE_VERSION} \
-	--output ${OUTPUT} \
+	-t "${GITHUB_TOKEN}" \
+	--since-tag "${VERSION}" \
+	--future-release "${RELEASE_VERSION}" \
+	--output "${OUTPUT}" \
 	--exclude-labels bumpversion
 
 # bump the version
-echo ${RELEASE_VERSION} > VERSION
+echo "${RELEASE_VERSION}" > VERSION
 
 # git commit
-git add VERSION docs/releases/${RELEASE_VERSION}.md
+git add VERSION "docs/releases/${RELEASE_VERSION}.md"
 git commit -m "bump version to ${RELEASE_VERSION}"
