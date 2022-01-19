39615d39fd8172af08ba2bbbdc4f5077621f240b
Jeffrey Chien <chienjef@amazon.com>
2022-01-19 13:33:10 -0500

    Fix performance model version. (#897)
    Clean up release-notes scripts.

--- tools/release/generate-release-note.sh
+++ tools/release/generate-release-note.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
@@ -11,14 +13,12 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-#! /bin/bash
-
-VERSION=$1
+VERSION=$(cat VERSION)
 
 sed "s/__VERSION__/$VERSION/g" tools/release/header.md.template > header
 sed "s/__VERSION__/$VERSION/g" tools/release/downloading-links.md.template > downloading-links
 # Formats changelog sections: **<label>:** becomes ### <label>
 # Skips the first 4 lines (title and version number)
-sed -e 1,4d -e "s/^\*\*\(.*\)\:\*\*/### \1/g" docs/releases/${VERSION}.md > changelog
+sed -e 1,4d -e "s/^\*\*\(.*\)\:\*\*/### \1/g" "docs/releases/${VERSION}.md" > changelog
 
 cat header changelog downloading-links > release-note
