19fca6f664280b10067080da1ba6c40b470c5ed1
Khanh Nguyen <khanhntd@amazon.com>
2022-01-31 12:41:33 -0500

    format sh files with indent 4

--- tools/release/bump-version-and-create-release-note.sh
+++ tools/release/bump-version-and-create-release-note.sh
@@ -24,13 +24,13 @@ OUTPUT="docs/releases/${RELEASE_VERSION}.md"
 
 # generate release note
 docker run --rm -it -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator \
-     --user aws-observability \
-     --project aws-otel-collector \
-     -t "${GITHUB_TOKEN}" \
-     --since-tag "${VERSION}" \
-     --future-release "${RELEASE_VERSION}" \
-     --output "${OUTPUT}" \
-     --exclude-labels bumpversion
+    --user aws-observability \
+    --project aws-otel-collector \
+    -t "${GITHUB_TOKEN}" \
+    --since-tag "${VERSION}" \
+    --future-release "${RELEASE_VERSION}" \
+    --output "${OUTPUT}" \
+    --exclude-labels bumpversion
 
 # bump the version
 echo "${RELEASE_VERSION}" >VERSION
