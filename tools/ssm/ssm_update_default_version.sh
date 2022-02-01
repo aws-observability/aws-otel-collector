bba01dcb0a4c36c50faa1ba6466840ebca9a945e
Khanh Nguyen <khanhntd@amazon.com>
2022-02-01 07:00:24 -0500

    Add ssm update default version and fix some problems with authentication

---
+++ tools/ssm/ssm_update_default_version.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+set -e
+
+##########################################
+# This script is used in CD workflow
+# to delete binaries which were uploaded to S3
+# when the validation test in CD are failing
+##########################################
+
+function error_exit() {
+    echo "$1" 1>&2
+    exit 1
+}
+
+function check_deps() {
+    test -f $(which aws) || error_exit "aws command not detected in path, please install it"
+    test -f $(which docker) || error_exit "docker command not detected in path, please install it"
+}
+
+function parse_environment_input() {
+    if [[ -z "${version}" ]]; then
+        error_exit "Missing input for flag version"
+    fi
+
+    if [[ -z "${ssm_package_name}" ]]; then
+        error_exit "Missing input for flag ssm_package_name"
+    fi
+}
+
+function update_second_largest_target_document_as_default() {
+  list_document_versions=$(aws ssm list-document-versions --name "${ssm_package_name}" --output json)
+  target_document_version=$(echo $list_document_versions | docker run --rm -i stedolan/jq -c -r ".DocumentVersions|.[]|select(.VersionName==\"${version}\")|.DocumentVersion")
+
+  if [[ -z $target_document_version ]]; then
+    error_exit "Document version not found for ${version}"
+  fi
+
+  second_largest_document_version="$((target_document_version-1))"
+  aws ssm describe-document --name ${ssm_package_name} --document-version "${second_largest_document_version}" >/dev/null && \
+    aws ssm update-document-default-version --name ${ssm_package_name} --document-version "${second_largest_document_version}"
+
+
+}
+check_deps
+parse_environment_input
+update_second_largest_target_document_as_default
+
+
