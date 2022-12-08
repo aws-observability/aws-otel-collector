09c05f065a8ad295d17e68c5789c9555eee75de4
Raphael Silva <rapphil@gmail.com>
2022-12-08 07:53:52 +0000

    Fix unit tests
    Signed-off-by: Raphael Silva <rapphil@gmail.com>

--- pkg/defaultcomponents/defaults_test.go
+++ pkg/defaultcomponents/defaults_test.go
@@ -22,8 +22,8 @@ import (
 )
 
 const (
-	exportersCount  = 13
-	receiversCount  = 8
+	exportersCount  = 14
+	receiversCount  = 9
 	extensionsCount = 7
 	processorCount  = 12
 )
@@ -49,6 +49,7 @@ func TestComponents(t *testing.T) {
 	assert.NotNil(t, exporters["signalfx"])
 	assert.NotNil(t, exporters["logzio"])
 	assert.NotNil(t, exporters["prometheusremotewrite"])
+	assert.NotNil(t, exporters["kafka"])
 
 	receivers := factories.Receivers
 	assert.Len(t, receivers, receiversCount)
@@ -63,6 +64,7 @@ func TestComponents(t *testing.T) {
 	assert.NotNil(t, receivers["prometheus"])
 	assert.NotNil(t, receivers["zipkin"])
 	assert.NotNil(t, receivers["jaeger"])
+	assert.NotNil(t, receivers["kafka"])
 
 	extensions := factories.Extensions
 	assert.Len(t, extensions, extensionsCount)
