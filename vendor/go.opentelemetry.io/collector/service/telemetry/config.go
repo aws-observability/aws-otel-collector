f91da3581b29a03312e8bc587e95f9a9c698bfc7
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2023-08-02 08:58:32 -0700

    Run go mod vendor to fix vendor issues

--- vendor/go.opentelemetry.io/collector/service/telemetry/config.go
+++ vendor/go.opentelemetry.io/collector/service/telemetry/config.go
@@ -9,6 +9,8 @@ import (
 	"go.uber.org/zap/zapcore"
 
 	"go.opentelemetry.io/collector/config/configtelemetry"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/internal/obsreportconfig"
 )
 
 // Config defines the configurable settings for service telemetry.
@@ -93,19 +95,6 @@ type LogsSamplingConfig struct {
 	Thereafter int `mapstructure:"thereafter"`
 }
 
-// MetricReader exposes configuration of metric readers to end users.
-// TODO: replace this temporary struct w/ auto-generated struct from jsonschema
-// https://github.com/open-telemetry/opentelemetry-configuration/tree/main/schema
-//
-// Experimental: *NOTE* this structure is subject to change or removal in the future.
-type MetricReader struct {
-	// Args corresponds to the JSON schema field "args".
-	Args any `mapstructure:"args"`
-
-	// Type corresponds to the JSON schema field "type".
-	Type string `mapstructure:"type"`
-}
-
 // MetricsConfig exposes the common Telemetry configuration for one component.
 // Experimental: *NOTE* this structure is subject to change or removal in the future.
 type MetricsConfig struct {
@@ -121,7 +110,7 @@ type MetricsConfig struct {
 
 	// Readers allow configuration of metric readers to emit metrics to
 	// any number of supported backends.
-	Readers []MetricReader `mapstructure:"metric_readers"`
+	Readers []MetricReader `mapstructure:"readers"`
 }
 
 // TracesConfig exposes the common Telemetry configuration for collector's internal spans.
@@ -131,15 +120,70 @@ type TracesConfig struct {
 	// tracecontext and  b3 are supported. By default, the value is set to empty list and
 	// context propagation is disabled.
 	Propagators []string `mapstructure:"propagators"`
+	// Processors allow configuration of span processors to emit spans to
+	// any number of suported backends.
+	Processors []SpanProcessor `mapstructure:"processors"`
 }
 
 // Validate checks whether the current configuration is valid
 func (c *Config) Validate() error {
-
 	// Check when service telemetry metric level is not none, the metrics address should not be empty
-	if c.Metrics.Level != configtelemetry.LevelNone && c.Metrics.Address == "" {
-		return fmt.Errorf("collector telemetry metric address should exist when metric level is not none")
+	if c.Metrics.Level != configtelemetry.LevelNone && c.Metrics.Address == "" && len(c.Metrics.Readers) == 0 {
+		return fmt.Errorf("collector telemetry metric address or reader should exist when metric level is not none")
 	}
 
 	return nil
+}
+
+func (sp *SpanProcessor) Unmarshal(conf *confmap.Conf) error {
+	if !obsreportconfig.UseOtelWithSDKConfigurationForInternalTelemetryFeatureGate.IsEnabled() {
+		// only unmarshal if feature gate is enabled
+		return nil
+	}
+
+	if conf == nil {
+		return nil
+	}
+
+	if err := conf.Unmarshal(sp); err != nil {
+		return fmt.Errorf("invalid span processor configuration: %w", err)
+	}
+
+	if sp.Batch != nil {
+		if sp.Batch.Exporter.Console == nil {
+			return fmt.Errorf("invalid exporter configuration")
+		}
+		return nil
+	}
+	return fmt.Errorf("unsupported span processor type %s", conf.AllKeys())
+}
+
+func (mr *MetricReader) Unmarshal(conf *confmap.Conf) error {
+	if !obsreportconfig.UseOtelWithSDKConfigurationForInternalTelemetryFeatureGate.IsEnabled() {
+		// only unmarshal if feature gate is enabled
+		return nil
+	}
+
+	if conf == nil {
+		return nil
+	}
+
+	if err := conf.Unmarshal(mr); err != nil {
+		return fmt.Errorf("invalid metric reader configuration: %w", err)
+	}
+
+	if mr.Pull != nil {
+		if mr.Pull.Exporter.Prometheus == nil {
+			return fmt.Errorf("invalid exporter configuration")
+		}
+		return nil
+	}
+	if mr.Periodic != nil {
+		if mr.Periodic.Exporter.Otlp == nil && mr.Periodic.Exporter.Console == nil {
+			return fmt.Errorf("invalid exporter configuration")
+		}
+		return nil
+	}
+
+	return fmt.Errorf("unsupported metric reader type %s", conf.AllKeys())
 }
