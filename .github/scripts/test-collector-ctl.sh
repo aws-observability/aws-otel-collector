3d239c5e4a620f14cf7b4882174e5a3dea856ed9
Raphael Philipe Mendes da Silva <rapphil@gmail.com>
2023-08-11 10:20:18 -0700

    Fix: don't overwrite .env file (#2261)
    * Fix: don't overwrite .env file
    
    Do not overwrite the .env file in case ctl commands are executed.
    This is specially important if extra environment variables are passed in the .env file
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Add tests for control script
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Fix typo in github workflow
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Fix shell linting errors
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Update .github/workflows/PR-build.yml
    
    * Make debian aligned with RPM
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * properly install collector deb
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Use dpkg to install collector
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Properly use cache
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Move control script tests to the CI
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    * Add the pr-build tests back
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    
    ---------
    
    Signed-off-by: Raphael Silva <rapphil@gmail.com>
    Co-authored-by: bryan-aguilar <46550959+bryan-aguilar@users.noreply.github.com>

---
+++ .github/scripts/test-collector-ctl.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -x
+
+set -e
+
+# This script is supposed to run during the CI to validate that the control
+# script is working as expected
+# Important: this script requires root permission.
+
+COLLECTOR_DEB=$1
+COLLECTOR_CONFIG=$2
+
+ADOT_CTL=/opt/aws/aws-otel-collector/bin/aws-otel-collector-ctl
+ENV_FILE="/opt/aws/aws-otel-collector/etc/.env"
+CONFIG_FILE="/opt/aws/aws-otel-collector/etc/config.yaml"
+
+setup() {
+    dpkg -i "$COLLECTOR_DEB"
+}
+
+test_collector_ctl_does_not_overwrite_env() {
+    $ADOT_CTL -a start
+    if [ ! -e $CONFIG_FILE ]; then
+        echo "Config file does not exist"
+        exit 1
+    fi
+
+    echo "EXTRA_ENV=\"some value\"" | tee -a $ENV_FILE > /dev/null
+
+    # We don't need the collector to succeed, just that the control script is execised
+    $ADOT_CTL -a start -c "http://example.com"
+
+    if ! grep -q EXTRA_ENV "$ENV_FILE"; then
+        echo "Env file was overwritten"
+        exit 1
+    fi
+
+    $ADOT_CTL -a start -c "$COLLECTOR_CONFIG"
+    if ! grep -q EXTRA_ENV "$ENV_FILE"; then
+        ehco "Env file was overwritten"
+        exit 1
+    fi
+    echo "${FUNCNAME[0]} ... OK"
+}
+
+
+setup
+
+## Tests
+test_collector_ctl_does_not_overwrite_env
