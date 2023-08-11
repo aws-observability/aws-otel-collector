a7c47d077099c92406da94018e9ebc8fcbfa3f1c
Raphael Silva <rapphil@gmail.com>
2023-08-11 00:22:59 +0000

    Fix shell linting errors
    Signed-off-by: Raphael Silva <rapphil@gmail.com>

--- .github/scripts/test-collector-ctl.sh
+++ .github/scripts/test-collector-ctl.sh
@@ -10,29 +10,27 @@ ADOT_CTL=/opt/aws/aws-otel-collector/bin/aws-otel-collector-ctl
 ENV_FILE="/opt/aws/aws-otel-collector/etc/.env"
 
 setup() {
-    sudo apt install $COLLECTOR_DEB
+    sudo apt install "$COLLECTOR_DEB"
 }
 
 test_collector_ctl_does_not_overwrite_env() {
     # Prepare
-    sudo $ADOT_CTL -a start -c $COLLECTOR_CONFIG
+    sudo $ADOT_CTL -a start -c "$COLLECTOR_CONFIG"
 
     # Act
     echo "EXTRA_ENV=\"some value\"" | sudo tee -a $ENV_FILE > /dev/null
 
     # Assert
     # We don't need the collector to succeed, just that the control script is execised
-    sudo $ADOT_CTL -a start -c http://example.com
+    sudo $ADOT_CTL -a start -c "http://example.com"
 
-    sudo cat $ENV_FILE | grep -q EXTRA_ENV
-    if [ $? -eq 0 ]; then
+    if ! sudo grep -q EXTRA_ENV "$ENV_FILE"; then
         echo "Env file was overwritten"
         exit 1
     fi
 
-    sudo $ADOT_CTL -a start -c $COLLECTOR_CONFIG
-    sudo cat $ENV_FILE | grep -q EXTRA_ENV
-    if [ $? -ne 0 ]; then
+    sudo $ADOT_CTL -a start -c "$COLLECTOR_CONFIG"
+    if ! sudo grep -q EXTRA_ENV "$ENV_FILE"; then
         ehco "Env file was overwritten"
         exit 1
     fi
