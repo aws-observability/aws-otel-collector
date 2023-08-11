5c2378a7495c698193e6ff7b19a87cf018f26a53
Raphael Silva <rapphil@gmail.com>
2023-08-11 03:12:29 +0000

    properly install collector deb
    Signed-off-by: Raphael Silva <rapphil@gmail.com>

--- .github/scripts/test-collector-ctl.sh
+++ .github/scripts/test-collector-ctl.sh
@@ -2,6 +2,7 @@
 
 # This script is supposed to run during the CI to validate that the control
 # script is working as expected
+# Important: this script requires root permission.
 
 COLLECTOR_DEB=$1
 COLLECTOR_CONFIG=$2
@@ -10,30 +11,31 @@ ADOT_CTL=/opt/aws/aws-otel-collector/bin/aws-otel-collector-ctl
 ENV_FILE="/opt/aws/aws-otel-collector/etc/.env"
 
 setup() {
-    sudo apt install "$COLLECTOR_DEB"
+    apt-get install "$COLLECTOR_DEB"
 }
 
 test_collector_ctl_does_not_overwrite_env() {
     # Prepare
-    sudo $ADOT_CTL -a start -c "$COLLECTOR_CONFIG"
+    $ADOT_CTL -a start -c "$COLLECTOR_CONFIG"
 
     # Act
-    echo "EXTRA_ENV=\"some value\"" | sudo tee -a $ENV_FILE > /dev/null
+    echo "EXTRA_ENV=\"some value\"" | tee -a $ENV_FILE > /dev/null
 
     # Assert
     # We don't need the collector to succeed, just that the control script is execised
-    sudo $ADOT_CTL -a start -c "http://example.com"
+    $ADOT_CTL -a start -c "http://example.com"
 
-    if ! sudo grep -q EXTRA_ENV "$ENV_FILE"; then
+    if ! grep -q EXTRA_ENV "$ENV_FILE"; then
         echo "Env file was overwritten"
         exit 1
     fi
 
-    sudo $ADOT_CTL -a start -c "$COLLECTOR_CONFIG"
-    if ! sudo grep -q EXTRA_ENV "$ENV_FILE"; then
+    $ADOT_CTL -a start -c "$COLLECTOR_CONFIG"
+    if ! grep -q EXTRA_ENV "$ENV_FILE"; then
         ehco "Env file was overwritten"
         exit 1
     fi
+    echo "${FUNCNAME[0]} ... OK"
 }
 
 
