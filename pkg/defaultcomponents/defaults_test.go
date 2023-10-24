8be11ed471ef4aa773c52ed27fb1fb14f94169e9
Vasi Vasireddy <pvasir@amazon.com>
2023-10-24 15:55:20 -0700

    Modify/remove the tests as components in stageBeta are added by default

--- pkg/defaultcomponents/defaults_test.go
+++ pkg/defaultcomponents/defaults_test.go
@@ -16,16 +16,15 @@
 package defaultcomponents
 
 import (
-	"go.opentelemetry.io/collector/featuregate"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	exportersCount  = 15
-	receiversCount  = 9
-	extensionsCount = 7
+	exportersCount  = 16
+	receiversCount  = 10
+	extensionsCount = 8
 	processorCount  = 15
 )
 
@@ -104,46 +103,8 @@ func TestComponents(t *testing.T) {
 	assert.NotNil(t, processors["tail_sampling"])
 	assert.NotNil(t, processors["k8sattributes"])
 
-	// Ensure that the components behind feature gates aren't included
-	assert.Nil(t, receivers["filelog"])
-	assert.Nil(t, exporters["awscloudwatchlogs"])
-	assert.Nil(t, extensions["file_storage"])
-}
-
-func TestEnabledFeatureGate(t *testing.T) {
-	testCases := []struct {
-		featureName string
-		expectedLen int
-	}{
-		{"adot.receiver.filelog", receiversCount + 1},
-		{"adot.exporter.awscloudwatchlogs", exportersCount + 1},
-		{"adot.extension.file_storage", extensionsCount + 1},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.featureName, func(t *testing.T) {
-			err := featuregate.GlobalRegistry().Set(tc.featureName, true)
-			assert.NoError(t, err)
-
-			factories, err := Components()
-			assert.NoError(t, err)
-
-			switch tc.featureName {
-			case "adot.receiver.filelog":
-				receivers := factories.Receivers
-				assert.Len(t, receivers, tc.expectedLen)
-				assert.NotNil(t, receivers["filelog"])
-
-			case "adot.exporter.awscloudwatchlogs":
-				exporters := factories.Exporters
-				assert.Len(t, exporters, tc.expectedLen)
-				assert.NotNil(t, exporters["awscloudwatchlogs"])
-
-			case "adot.extension.file_storage":
-				extensions := factories.Extensions
-				assert.Len(t, extensions, tc.expectedLen)
-				assert.NotNil(t, extensions["file_storage"])
-			}
-		})
-	}
+	// Ensure that the components behind feature gates are included in stageBeta by default
+	assert.NotNil(t, receivers["filelog"])
+	assert.NotNil(t, exporters["awscloudwatchlogs"])
+	assert.NotNil(t, extensions["file_storage"])
 }
