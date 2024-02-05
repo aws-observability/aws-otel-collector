205370ea44d805b96edceea380d0ec57a47074de
Vasi Vasireddy <pvasir@amazon.com>
2024-02-04 22:26:55 -0800

    Remove Prometheusremotewrite patch

--- vendor/go.opentelemetry.io/collector/exporter/exporterhelper/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-// Package exporterhelper provides helper functions for exporters.
-package exporterhelper // import "go.opentelemetry.io/collector/exporter/exporterhelper"
