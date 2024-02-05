6dc93808192588fcdc5c3339fb4a09fe68d38ebd
Vasi Vasireddy <pvasir@amazon.com>
2024-02-04 23:14:29 -0800

    Removed patches

--- vendor/go.opentelemetry.io/collector/extension/zpagesextension/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-//go:generate mdatagen metadata.yaml
-
-// Package zpagesextension implements an extension that exposes zPages of
-// properly instrumented components.
-package zpagesextension // import "go.opentelemetry.io/collector/extension/zpagesextension"
