b48bc2416d3d932e4868fa02069367f22cbe335d
Vasi Vasireddy <pvasir@amazon.com>
2025-07-30 21:56:11 +0000

    Split EKS batches in multiple targets for easy sequentials execs

--- e2etest/get-testcases.py
+++ e2etest/get-testcases.py
@@ -29,8 +29,21 @@ if __name__ == "__main__":
         "arm_ubuntu16"
     ]}
     ecs_matrix = {"testcase": [], "launch_type": ["EC2", "FARGATE"]}
-    eks_matrix = {"testcase": []}
-    eks_arm64_matrix = {"testcase": []}
+    
+    # New EKS matrices for different versions
+    eks_127_matrix = {"testcase": []}
+    eks_128_matrix = {"testcase": []}
+    eks_129_matrix = {"testcase": []}
+    eks_130_matrix = {"testcase": []}
+    eks_131_matrix = {"testcase": []}
+    
+    # New EKS ARM64 matrices for different versions
+    eks_arm64_127_matrix = {"testcase": []}
+    eks_arm64_128_matrix = {"testcase": []}
+    eks_arm64_129_matrix = {"testcase": []}
+    eks_arm64_130_matrix = {"testcase": []}
+    eks_arm64_131_matrix = {"testcase": []}
+    
     eks_fargate_matrix = {"testcase": []}
     eks_adot_operator_matrix = {"testcase": []}
     local_matrix = {"testcase": []}
@@ -42,8 +55,16 @@ if __name__ == "__main__":
             "ec2_matrix_2": ec2_matrix_2,
             "ec2_matrix_3": ec2_matrix_3,
             "ecs_matrix": ecs_matrix,
-            "eks_matrix": eks_matrix,
-            "eks_arm64_matrix": eks_arm64_matrix,
+            "eks_127_matrix": eks_127_matrix,
+            "eks_128_matrix": eks_128_matrix,
+            "eks_129_matrix": eks_129_matrix,
+            "eks_130_matrix": eks_130_matrix,
+            "eks_131_matrix": eks_131_matrix,
+            "eks_arm64_127_matrix": eks_arm64_127_matrix,
+            "eks_arm64_128_matrix": eks_arm64_128_matrix,
+            "eks_arm64_129_matrix": eks_arm64_129_matrix,
+            "eks_arm64_130_matrix": eks_arm64_130_matrix,
+            "eks_arm64_131_matrix": eks_arm64_131_matrix,
             "eks_fargate_matrix": eks_fargate_matrix,
             "eks_adot_operator_matrix": eks_adot_operator_matrix,
             "local_matrix": local_matrix,
@@ -60,10 +81,28 @@ if __name__ == "__main__":
                 ec2_matrix_3["testcase"].append(testcase["case_name"])
             if 'ECS' in testcase["platforms"]:
                 ecs_matrix["testcase"].append(testcase["case_name"])
-            if 'EKS' in testcase["platforms"]:
-                eks_matrix["testcase"].append(testcase["case_name"])
-            if 'EKS_ARM64' in testcase["platforms"]:
-                eks_arm64_matrix["testcase"].append(testcase["case_name"])
+            # Handle version-specific EKS platforms
+            if 'EKS_127' in testcase["platforms"]:
+                eks_127_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_128' in testcase["platforms"]:
+                eks_128_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_129' in testcase["platforms"]:
+                eks_129_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_130' in testcase["platforms"]:
+                eks_130_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_131' in testcase["platforms"]:
+                eks_131_matrix["testcase"].append(testcase["case_name"])
+            # Handle version-specific EKS ARM64 platforms
+            if 'EKS_ARM64_127' in testcase["platforms"]:
+                eks_arm64_127_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_ARM64_128' in testcase["platforms"]:
+                eks_arm64_128_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_ARM64_129' in testcase["platforms"]:
+                eks_arm64_129_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_ARM64_130' in testcase["platforms"]:
+                eks_arm64_130_matrix["testcase"].append(testcase["case_name"])
+            if 'EKS_ARM64_131' in testcase["platforms"]:
+                eks_arm64_131_matrix["testcase"].append(testcase["case_name"])
             if 'EKS_FARGATE' in testcase["platforms"]:
                 eks_fargate_matrix["testcase"].append(testcase["case_name"])
             if 'EKS_ADOT_OPERATOR' in testcase["platforms"]:
